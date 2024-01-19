bd3af65892f99892e8555e7d253afcf4cf5baac7
fabiojuliosantos <fabiojulio.santos@protonmail.ch>
2024-01-19 09:02:11 -0300

    Teste de commit

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -16,6 +16,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
